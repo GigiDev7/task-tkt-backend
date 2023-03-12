70dabe3b47c6e58166c16a46f1a2eac338cb3a5a
GigiDev7 <gigivadachkoria@gmail.com>
2023-03-12 15:55:17 +0400

    bug fix

--- services/events.js
+++ services/events.js
@@ -3,16 +3,25 @@ const Event = require("../models/eventSchema");
 exports.findEvents = (query) => {
   const filters = [];
   if (query.date) {
-    filters.push({ date: new Date(query.date) });
+    filters.push(
+      { year: new Date(query.date).getFullYear() },
+      { month: new Date(query.date).getMonth() + 1 }
+    );
   }
   if (query.title) {
     filters.push({ title: { $regex: query.title, $options: "i" } });
   }
   return Event.aggregate([
     {
+      $addFields: { month: { $month: "$date" }, year: { $year: "$date" } },
+    },
+    {
       $match: {
         $and: [...filters],
       },
+    },
+    {
+      $unset: ["month", "year"],
     },
   ]);
 };
